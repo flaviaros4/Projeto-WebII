2e3ea03dab5f237735925892c638a145e83c56af
flaviaros4 <flaviarosa8@gmail.com>
2025-08-19 09:34:32 -0300

    rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { Cadastro } from './pages/cadastro/cadastro';
+import { App } from './app';
 
 export const routes: Routes = [
   { path: 'cadastro', component: Cadastro },
