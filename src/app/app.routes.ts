2f9ee9e7a0bc95db2bff9b4b5cd98ef2b7004a12
Leticia Sanches <leticiasanchessilv@gmail.com>
2025-08-19 09:47:21 -0300

    update login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from '@angular/router';
 
 import { Cadastro } from './pages/cadastro/cadastro';
 
+import { Login } from './pages/login/login';
+
 export const routes: Routes = [
+  { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'cadastro', component: Cadastro },
+  { path: 'login', component: Login}
 ];
