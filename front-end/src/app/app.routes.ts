c6314a8b5dc25b9970bac394aba96763716130cb
Leon Trindade <leon.trindade10@gmail.com>
2025-08-24 21:41:54 -0300

    melhorado o estilo da pagina de solicitação

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -3,9 +3,12 @@ import { Routes } from '@angular/router';
 import { Cadastro } from './pages/cadastro/cadastro';
 
 import { Login } from './pages/login/login';
+import { SolicitacaoDeManutencao } from './pages/solicitacao-de-manutencao/solicitacao-de-manutencao';
 
 export const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
   { path: 'cadastro', component: Cadastro },
-  { path: 'login', component: Login}
+  { path: 'login', component: Login},
+  {path: 'solicitacao', component:SolicitacaoDeManutencao}
+
 ];
