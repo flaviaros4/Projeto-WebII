9b444d9400e7e94a4d57cb7ccd938e3957239dbf
Leon Trindade <leon.trindade10@gmail.com>
2025-09-16 14:22:21 -0300

    interface pagina do funcionário

--- front-end/src/app/pages/listar-funcionarios/listar-funcionarios.ts
+++ front-end/src/app/pages/listar-funcionarios/listar-funcionarios.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface Funcionario {
+  id: number;
+  nome: string;
+  idade: number;
+}
 
 @Component({
   selector: 'app-listar-funcionarios',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './listar-funcionarios.html',
-  styleUrl: './listar-funcionarios.css'
+  styleUrls: ['./listar-funcionarios.css']
 })
 export class ListarFuncionarios {
 
+  funcionarios: Funcionario[] = [
+    { id: 1, nome: 'Maria Silva', idade: 30 },
+    { id: 2, nome: 'João Souza', idade: 40 },
+    { id: 3, nome: 'Ana Pereira', idade: 25 }
+  ];
+
+  novo() {
+    alert('Clique em Novo');
+  }
+
+  editar(id: number) {
+    alert('Editar funcionário ' + id);
+  }
+
+  remover(id: number) {
+    alert('Remover funcionário ' + id);
+  }
 }
